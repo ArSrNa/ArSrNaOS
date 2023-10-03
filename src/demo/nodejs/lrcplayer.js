90ea47b28a92fa37412ccb2d8a71e505329f8447
ArSrNa <arsrna.cn@outlook.com>
2023-10-03 19:23:05 +0800

    宽高问题

--- src/demo/nodejs/lrcplayer.js
+++ src/demo/nodejs/lrcplayer.js
@@ -1,7 +1,7 @@
 export function LrcPlayer() {
     return (<>
         <iframe src="https://codesandbox.io/embed/arsrnalrcplayer-7nwcp3?fontsize=14&hidenavigation=1&theme=light"
-            style={{ width: '100%', height: 500, border: 0, borderRadius: 4, overflow: 'hidden', }}
+            style={{ width: '100%', minHeight: '90vh', border: 0, borderRadius: 4, overflow: 'hidden', }}
             title="ArSrNaLRCPlayer"
             allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
             sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
